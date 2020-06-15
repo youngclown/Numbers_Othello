f66299de35659990eb195244dad1f623e686537f
crown0047 <ku9cu@knou.ac.kr>
2020-06-15 17:26:53 +0900

    member

--- src/main/java/com/chat/number/NumberGoApplication.java
+++ src/main/java/com/chat/number/NumberGoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NumberGoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NumberGoApplication.class, args);
     }
-
 }
