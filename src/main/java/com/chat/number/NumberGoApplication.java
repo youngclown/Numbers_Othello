19e22e4caa6ec85a8886ede6f9c1e5861d4e8223
crown0047 <ku9cu@knou.ac.kr>
2020-06-15 15:25:36 +0900

    로그인 계정 관련 코드값

--- src/main/java/com/chat/number/NumberGoApplication.java
+++ src/main/java/com/chat/number/NumberGoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TabulaApplication {
+public class NumberGoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TabulaApplication.class, args);
+        SpringApplication.run(NumberGoApplication.class, args);
     }
 
 }
